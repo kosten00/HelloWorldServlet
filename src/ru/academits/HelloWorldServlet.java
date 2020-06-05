5ade4b7c5091f1b6fb3e89cd4e22693c3eb59e89
kosten00 <ylkahu@gmail.com>
2020-06-05 14:21:39 +0700

    Added form for html document. Added post method.

--- src/ru/academits/HelloWorldServlet.java
+++ src/ru/academits/HelloWorldServlet.java
@@ -7,8 +7,22 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class HelloWorldServlet extends HttpServlet {
+    private final String DEFAULT_STRING = "Hello from HelloWorldServlet";
+
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getOutputStream().write("Hello from HelloWorldServlet".getBytes(StandardCharsets.UTF_8));
+        resp.getOutputStream().write(DEFAULT_STRING.getBytes(StandardCharsets.UTF_8));
+        resp.getOutputStream().flush();
+        resp.getOutputStream().close();
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String postedString = req.getParameter("postedString");
+
+        if (postedString.length() == 0) {
+            postedString = DEFAULT_STRING;
+        }
+
+        resp.getOutputStream().write(postedString.getBytes(StandardCharsets.UTF_8));
         resp.getOutputStream().flush();
         resp.getOutputStream().close();
     }
